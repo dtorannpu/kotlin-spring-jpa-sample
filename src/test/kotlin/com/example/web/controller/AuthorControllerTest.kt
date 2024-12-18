2768b893d597555faf217a1febbd6947f8c8bcd0
dtorannpu <dtorannpu@gmail.com>
2024-12-18 12:51:53 +0900

    @MockBeanを@MockitoBeanに変更
    @MockBeanが非推奨となったため

--- src/test/kotlin/com/example/web/controller/AuthorControllerTest.kt
+++ src/test/kotlin/com/example/web/controller/AuthorControllerTest.kt
@@ -11,8 +11,8 @@ import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.delete
 import org.springframework.test.web.servlet.get
@@ -24,7 +24,7 @@ class AuthorControllerTest {
     @Autowired
     private lateinit var mvc: MockMvc
 
-    @MockBean
+    @MockitoBean
     private lateinit var authorService: AuthorService
 
     @Test
